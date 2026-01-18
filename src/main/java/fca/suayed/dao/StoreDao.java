867ea0557e9e18ceeaebb1f7b84131e65155cca7
DanC271 <crivellizdany@gmail.com>
2026-01-17 19:19:32 -0600

    Update StoreDao.java

--- src/main/java/fca/suayed/dao/StoreDao.java
+++ src/main/java/fca/suayed/dao/StoreDao.java
@@ -1,6 +1,6 @@
 package fca.suayed.dao;
 
-
+import com.tiempor3al.dto.ClientDto;
 import fca.suayed.dto.ProductDto;
 import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
 import org.jdbi.v3.sqlobject.customizer.BindBean;
@@ -17,9 +17,8 @@ public interface StoreDao {
 
     @SqlUpdate("INSERT INTO productos (nombre, descripcion, precio, cantidad, sku) VALUES(:p.name, :p.description, :p.price, :p.quantity, :p.sku)")
     void addProduct(@BindBean("p") ProductDto productDto);
-    @RegisterBeanMapper(ClientDto.class) 
+    @RegisterBeanMapper(ClientDto.class)
     @SqlQuery("SELECT * FROM clientes")
     List<ClientDto> getClients();
-
 
 }
